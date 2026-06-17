07d51412a7d1f2ae2411bd0416a83f8e004019dc
robocoprobo7786-lang <robocoprobo7786@gmail.com>
2026-06-17 16:37:13 +0530

    Third commit

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -8,9 +8,13 @@ import Image from "next/image";
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className={styles.globe} />
+      <div className="rounded-lg bg-blue-500 p-6 text-white mb-4 h-40 flex flex-col justify-end">
+        <div className="max-w-7xl mt-auto">
+          <AcmeLogo />
+        </div>
+      </div>
+      {/* <div className={styles.globe} /> */}
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <AcmeLogo />
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black" />
           <p
